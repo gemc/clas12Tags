07a9fbf15ca31aa4120da1b3bd4ff32512d3d720
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-09-11 14:13:31 -0400

    time_in_ns in ctof is not an int

--- source/gemc.cc
+++ source/gemc.cc
@@ -28,7 +28,7 @@
 /// \author \n &copy; Maurizio Ungaro
 /// \author e-mail: ungaro@jlab.org\n\n\n
 
-const char *GEMC_VERSION = "gemc 5.4" ;
+const char *GEMC_VERSION = "gemc 5.5" ;
 
 // G4 headers
 #include "G4RunManager.hh"
