9a6d5429683ed88ce25c5245f4f4b58caae6fce1
Maurizio Ungaro <maureeungaro@gmail.com>
2025-01-16 14:42:11 -0500

    added run variation reports

---
+++ ci/report_runs_and_variations.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+runs=(11 3029 4763)
+variations=(default rga_spring2018_mc rga_fall2018_mc rga_spring2019_mc
+	rgb_fall2019_mc rgb_spring2019_mc rgc_summer2022_mc rgf_spring2020_mc rgm_fall2021_mc)
+
+
+echo "| Run |"
+echo "| --- |"
+for run in $=runs; do
+	echo "| $run |"
+done
+
+echo
+echo "<br/>"
+echo
+
+echo "| Variation |"
+echo "| --- |"
+for variation in $=variations; do
+	echo "| $variation |"
+done
