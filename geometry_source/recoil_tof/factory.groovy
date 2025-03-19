87a068648bb0a9eb19c1a2cdea280e9199e12323
niwgit <wnilanga@yahoo.com>
2025-03-19 10:50:36 -0400

    Implementation of recoil TOF geometry for RGH

---
+++ geometry_source/recoil_tof/factory.groovy
@@ -0,0 +1,24 @@
+//============================================================
+// This script prints volumes for RECOIL detector
+//============================================================
+import org.jlab.geom.base.*;
+import org.jlab.clasrec.utils.*;
+import org.jlab.detector.geant4.v2.recoil_tof.*;
+import org.jlab.detector.base.*;
+
+import GeoArgParse
+def variation = GeoArgParse.getVariation(args);
+def runNumber = GeoArgParse.getRunNumber(args);
+
+
+ConstantProvider cp = GeometryFactory.getConstants(DetectorType.FTOF,runNumber,"default");
+//cp.show();
+
+RecoilTOFGeant4Factory factory = new RecoilTOFGeant4Factory(cp);
+
+def outFile = new File("recoil_tof__volumes_"+variation+".txt");
+
+outFile.newWriter().withWriter { w ->
+	w<<factory;
+}
+
---
+++ geometry_source/recoil_tof/geometry_java.pl
@@ -0,0 +1,85 @@
+package coatjava;
+
+use strict;
+use warnings;
+
+use geometry;
+my $mothers;
+my $positions;
+my $rotations;
+my $types;
+my $dimensions;
+my $ids;
+ 
+sub make_sector
+{
+    my $nSectors = 2;
+    my $nRows = 5;
+    my $nCols = 63;
+
+    
+    for(my $s=1; $s<=$nSectors; $s++)
+    {
+	my %detector = init_det();
+	my $vname = "recoil_tof_sector$s";
+        
+	$detector{"name"}        = $vname;
+	$detector{"mother"}      = $mothers->{$vname};
+	$detector{"description"} = "CLAS12 recoil TOF, Sector $s";
+	$detector{"pos"}         = $positions->{$vname};
+	$detector{"rotation"}    = $rotations->{$vname};
+	$detector{"type"}        = $types->{$vname};
+	$detector{"dimensions"}  = $dimensions->{$vname};
+	$detector{"color"}       = "aa0000";
+	$detector{"material"}    = "G4_Galactic";
+	$detector{"style"}       = 0;
+	$detector{"visible"}     = 0;
+	print_det(\%main::configuration, \%detector);
+
+	for(my $row=1; $row<=$nRows; $row++) {
+	    for(my $col=1; $col<=$nCols; $col++) {
+		make_bar($s, $row, $col);
+    	    }
+	}
+    }	
+    
+}
+
+sub make_bar
+{
+    my $iSector = $_[0];
+    my $iRow = $_[1];
+    my $iCol =$_[2];
+    
+    my %detector = init_det();
+
+    my $vname = "bar_sector${iSector}_row${iRow}_column${iCol}";
+
+    $detector{"name"}        = $vname;
+    $detector{"mother"}      = $mothers->{$vname};
+    $detector{"description"} = "Sector $iSector, Bar, row $iRow, column $iCol";
+    $detector{"pos"}         = $positions->{$vname};
+    $detector{"rotation"}    = $rotations->{$vname};
+    $detector{"type"}        = $types->{$vname};
+    $detector{"dimensions"}  = $dimensions->{$vname};
+    $detector{"color"}    = "aa0000";
+    $detector{"material"} = "scintillator";
+    $detector{"style"}    = 0;
+    $detector{"visible"}  = 1;
+
+    print_det(\%main::configuration, \%detector);
+        
+}
+
+
+sub makeRECOILTOF
+{
+
+    ($mothers, $positions, $rotations, $types, $dimensions, $ids) = @main::volumes;
+   
+    make_sector();
+
+
+}
+
+1;
