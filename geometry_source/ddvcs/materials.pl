3861ac0c91756fe60ebb55bd72087ac6a539ecc0
Raffaella De Vita <devita@ge.infn.it>
2025-04-04 12:26:37 -0400

    switching default ddvcs material to lead

--- geometry_source/ddvcs/materials.pl
+++ geometry_source/ddvcs/materials.pl
@@ -7,23 +7,23 @@ sub materials
 {
 	# uploading the mat definition
 	
-	
-   # Beamline_Tungsten
-   my %mat = init_mat();
-   $mat{"name"}          = "beamline_W";
-   $mat{"description"}   = "beamline tungsten alloy 17.6 g/cm3";
-   $mat{"density"}       = "17.6";
-   $mat{"ncomponents"}   = "2";
-   $mat{"components"}    = "G4_Fe 0.08 G4_W 0.92";
-   print_mat(\%configuration, \%mat);
 
-	%mat = init_mat();
-   $mat{"name"}          = "ddvcs_W";
-   $mat{"description"}   = "ddvcs tungsten alloy 17.6 g/cm3";
-   $mat{"density"}       = "17.6";
-   $mat{"ncomponents"}   = "2";
-   $mat{"components"}    = "G4_Fe 0.08 G4_W 0.92";
-   print_mat(\%configuration, \%mat);
+        # Beamline_Tungsten
+        my %mat = init_mat();
+        $mat{"name"}          = "beamline_W";
+        $mat{"description"}   = "beamline tungsten alloy 17.6 g/cm3";
+        $mat{"density"}       = "17.6";
+        $mat{"ncomponents"}   = "2";
+        $mat{"components"}    = "G4_Fe 0.08 G4_W 0.92";
+        print_mat(\%configuration, \%mat);
+
+            %mat = init_mat();
+        $mat{"name"}          = "ddvcs_shield_mat";
+        $mat{"description"}   = "ddvcs shield material"};
+        $mat{"density"}       = "11.34";
+        $mat{"ncomponents"}   = "1";
+        $mat{"components"}    = "G4_Pb";
+        print_mat(\%configuration, \%mat);
 
 	# rohacell
 	$mat{"name"}          = "rohacell";
