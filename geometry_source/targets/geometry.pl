51e70e06ad6b8e00d90b6d17b2186c9715f70bc7
Mathieu Ouillon <mathieu.ouillon1@gmail.com>
2025-08-26 15:54:15 -0400

    add rgd_fall2023_lD2 configuration for liquid standard targets

--- geometry_source/targets/geometry.pl
+++ geometry_source/targets/geometry.pl
@@ -52,7 +52,8 @@ sub build_target {
         or $configuration_string eq "rga_spring2019"
         or $configuration_string eq "rgb_fall2019"
         or $configuration_string eq "rgm_fall2021_He"
-        or $configuration_string eq "lH2e") {
+        or $configuration_string eq "lH2e"
+        or $configuration_string eq "rgd_fall2023_lD2") {
         build_liquid_standards();
     }
     elsif ($configuration_string eq "rgf_spring2020" ||
