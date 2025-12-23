5c7e2d0821de5be935813998c1c98472717f63f5
Maurizio Ungaro <maureeungaro@gmail.com>
2025-12-23 09:19:16 -0500

    cloning repo tag with submodules

--- geometry_source/install_coatjava.sh
+++ geometry_source/install_coatjava.sh
@@ -51,7 +51,7 @@ fi
 # print help if -h is given
 if [[ $1 == "-h" ]]; then
 	echo "Usage: install_coatjava.sh [-d] [-t tag]"
-	echo "  -d: use coatjava development version"
+	echo "  -d: use coatjava development version (github main)"
 	echo "  -t tag: use coatjava tag version"
 	echo "  -g github_url: use a custom github url"
 	echo "  -l use latest tag"
@@ -69,16 +69,7 @@ if [[ $USEDEVEL == "yes" ]]; then
 	echo
 	git clone $githubRepo $src_dir
 else
-	# extract filename from $tag_gz
-	coatJavaFileName=$(echo $tag_gz | sed 's/.*\///')
-	echo "Downloading coatjava version $tag_gz onto $coatJavaFileName"
-	mwget --trust-server-names $tag_gz -O $coatJavaFileName
-
-	echo "Unpacking...$coatJavaFileName"
-	tar -xf $coatJavaFileName
-	rm -f $coatJavaFileName
-
-	mv coatjava-$COATJAVA_TAG $src_dir
+	git clone  -b $COATJAVA_TAG --recurse-submodules --shallow-submodules --depth 1 $githubRepo $src_dir
 fi
 
 # recent versions of Git refuse to touch a repository whose on-disk owner
@@ -90,7 +81,7 @@ git config --global --add safe.directory '*'
 cd $src_dir
 paralllel=" -T"$(getconf _NPROCESSORS_ONLN)
 paralllel=" -T1"
-echo "Running coatjava build with options: --no-progress  $paralllel" >> ../build_coatjava.log | tee -a ../build_coatjava.log
+echo "Running coatjava build with options: --lfs --no-progress --nomaps  $paralllel" >> ../build_coatjava.log | tee -a ../build_coatjava.log
 ./build-coatjava.sh --lfs --no-progress --nomaps  $paralllel &>> ../build_coatjava.log
 if [[ $? -ne 0 ]]; then
 	echo "Error: coatjava build failed. Log:"
