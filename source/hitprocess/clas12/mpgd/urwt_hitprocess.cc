813c900902b06279c74e2653f12cee1078d25ca3
Raffaella De Vita <devita@ge.infn.it>
2026-03-06 17:55:52 -0500

    increased threshold on URWT and MUVT

--- source/hitprocess/clas12/mpgd/urwt_hitprocess.cc
+++ source/hitprocess/clas12/mpgd/urwt_hitprocess.cc
@@ -116,7 +116,7 @@ map<string, double> urwt_HitProcess::integrateDgt(MHit *aHit, int hitn)
   dgtz["ADC_time"] = identity[3].time;
   dgtz["ADC_ped"] = 0;
 
-  if((1.0 * (int)(urwtC.gain * 1e6 * tInfos.eTot / urwtC.w_i))<2){
+  if(1e6 * tInfos.eTot / urwtC.w_i < 10){
       rejectHitConditions =true;
   }
 
