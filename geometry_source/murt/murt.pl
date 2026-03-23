29cb2cc285a599c934c2f21a8a9d5ca4236a0254
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-23 13:24:25 -0400

    release 5.14

--- geometry_source/murt/murt.pl
+++ geometry_source/murt/murt.pl
@@ -79,7 +79,6 @@ foreach my $variation (@variations) {
     my $system = $configuration{'detector_name'};
     upload_parameters(\%configuration, "$system"."__parameters_$variation.txt", "$system", "default", $runNumber);
 }
-upload_parameters(\%configuration, "micromegas__parameters_michel_9mmcopper.txt", "micromegas", "default", 30000);
 
 foreach my $run (@runs) {
     $configuration{"variation"} = "default";
