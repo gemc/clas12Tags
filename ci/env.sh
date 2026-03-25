7ea3e94a7afa017e3416e5ebb497ec9ee0fc93d1
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-25 11:47:19 -0400

    creating gemc dir before using it and temp using only ddvcs - part 2

--- ci/env.sh
+++ ci/env.sh
@@ -233,8 +233,8 @@ else
 
 	export GEMC=$(pwd)
 	export GEMC_DATA_DIR=$GEMC
-	if [[ ! -d $GEMC ]] then;
-		mkdir -p $GEMC/bin
+	if [[ ! -d "$GEMC/bin" ]]; then
+		mkdir -p "$GEMC/bin"
 	fi
 	echo "Setting GEMC and GEMC_DATA_DIR to this directory: $GEMC"
 	export ARTIFACT_DIR=/cvmfs/oasis.opensciencegrid.org/jlab/geant4
