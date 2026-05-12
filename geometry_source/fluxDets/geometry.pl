7a4d25138f48a80fab2273459e850be396291c85
Raffaella De Vita <devita@ge.infn.it>
2026-05-12 19:28:26 -0400

    updated ddvcs flux detectors

--- geometry_source/fluxDets/geometry.pl
+++ geometry_source/fluxDets/geometry.pl
@@ -43,88 +43,87 @@ sub makeFlux {
     }
     elsif ($configuration{"variation"} eq "ddvcs") {
 
-        # tube that contains the scattering chamber
-        my $thickness = 1;
-        my $inner_radius = 100;
-        my $length = 650;
-        my $zpos = 175;
         my $material = "G4_Galactic";
 
+
+	# pokyvone in the central region
+        my $central_nplanes = 3;
+        my @central_iradius = (250.0, 250.0, 57.6);
+        my @central_oradius = (250.1, 250.1, 57.7);
+        my @central_zpos_root = (-400.0, 333.0, 416.0);
+    
+        my $dimen = "0.0*deg 360*deg $central_nplanes*counts";
+        for(my $i = 0; $i <$central_nplanes; $i++) {$dimen = $dimen ." $central_iradius[$i]*mm";}
+        for(my $i = 0; $i <$central_nplanes; $i++) {$dimen = $dimen ." $central_oradius[$i]*mm";}
+        for(my $i = 0; $i <$central_nplanes; $i++) {$dimen = $dimen ." $central_zpos_root[$i]*mm";}
+    
         my %detector = init_det();
-        $detector{"name"} = "ddvcs_central_tube_flux";
-        $detector{"mother"} = "root";
-        $detector{"description"} = "ddvcs central tube flux";
-        $detector{"pos"} = "0.0*cm 0.0*cm $zpos*mm";
-        $detector{"color"} = "aa0088";
-        $detector{"type"} = "Tube";
-        $detector{"dimensions"} = "$inner_radius*mm " . ($inner_radius + $thickness) . "*mm " . ($length / 2) . "*mm 0.*deg 360.*deg";
-        $detector{"material"} = $material;
-        $detector{"style"} = 1;
+    	$detector{"name"}        = "ddvcs_central_flux";
+    	$detector{"mother"}      = "root";
+    	$detector{"description"} = "ddvcs central flux";
+    	$detector{"color"}       = "aa0088";
+    	$detector{"type"}        = "Polycone";
+    	$detector{"dimensions"}  = $dimen;
+        $detector{"pos"}         = "0.0*cm 0.0*cm 0.0*cm";
+        $detector{"material"}    = $material;
+        $detector{"style"}       = 1;
         $detector{"sensitivity"} = "flux";
-        $detector{"hit_type"} = "flux";
+        $detector{"hit_type"}    = "flux";
         $detector{"identifiers"} = "id manual 1";
         print_det(\%configuration, \%detector);
 
 
-        # disk at the end of scattering chamber
-        my $outer_radius = 100;
-        $inner_radius = 58;
-        $length = 1;
-        $zpos = 500;
-
+	# cone after the shield
+        my $shield_nplanes = 2;
+        my @shield_iradius = (882.6, 150.2);
+        my @shield_oradius = (882.7, 150.3);
+        my @shield_zpos_root = (1132.3, 1473.8);
+    
+        $dimen = "0.0*deg 360*deg $shield_nplanes*counts";
+        for(my $i = 0; $i <$shield_nplanes; $i++) {$dimen = $dimen ." $shield_iradius[$i]*mm";}
+        for(my $i = 0; $i <$shield_nplanes; $i++) {$dimen = $dimen ." $shield_oradius[$i]*mm";}
+        for(my $i = 0; $i <$shield_nplanes; $i++) {$dimen = $dimen ." $shield_zpos_root[$i]*mm";}
+    
         %detector = init_det();
-        $detector{"name"} = "ddvcs_central_disk_flux";
-        $detector{"mother"} = "root";
-        $detector{"description"} = "ddvcs central disk flux";
-        $detector{"pos"} = "0.0*cm 0.0*cm $zpos*mm";
-        $detector{"color"} = "aa0088";
-        $detector{"type"} = "Tube";
-        $detector{"dimensions"} = "$inner_radius*mm $outer_radius*mm " . ($length / 2) . "*mm 0.*deg 360.*deg";
-        $detector{"material"} = $material;
-        $detector{"style"} = 1;
+    	$detector{"name"}        = "ddvcs_shield_flux";
+    	$detector{"mother"}      = "root";
+    	$detector{"description"} = "ddvcs shield flux";
+    	$detector{"color"}       = "aa0088";
+    	$detector{"type"}        = "Polycone";
+    	$detector{"dimensions"}  = $dimen;
+        $detector{"pos"}         = "0.0*cm 0.0*cm 0.0*cm";
+        $detector{"material"}    = $material;
+        $detector{"style"}       = 1;
         $detector{"sensitivity"} = "flux";
-        $detector{"hit_type"} = "flux";
+        $detector{"hit_type"}    = "flux";
         $detector{"identifiers"} = "id manual 2";
         print_det(\%configuration, \%detector);
 
-        # six sectors of fluxes just before FTOF
-
-        # my $panel1b_mother_dx1 = 8.635;
-        # my $panel1b_mother_dx2 = 200;
-        # my $panel1b_mother_dz = 180;
-        # my $panel1b_mother_dy = 0.01; # 0.1 mm
-        #
-        # # loop over sectors
-        # for (my $isect = 0; $isect < 6; $isect++) {
-        #
-        #     my $sector = $isect + 1;
-        #     my $identifier = 10 + $sector;
-        #
-        #     my $zrot = 90 - 60.0 * $isect;
-        #     my $xrot = 90.0 + 25;
-        #
-        #     my $position = sector_pos($isect);
-        #     my $rotation = "ordered: zxy $zrot*deg $xrot*deg 0*deg";
-        #
-        #     %detector = init_det();
-        #
-        #     $detector{"name"} = "ddvcs_forward_trap_sector$sector";
-        #     $detector{"mother"} = "root";
-        #     $detector{"description"} = "ddvcs forward trap flux sector $sector";
-        #     $detector{"pos"} = $position;
-        #     $detector{"rotation"} = $rotation;
-        #     $detector{"color"} = "222299";
-        #     $detector{"type"} = "Trd";
-        #     $detector{"dimensions"} = "$panel1b_mother_dx1*cm $panel1b_mother_dx2*cm $panel1b_mother_dy*cm $panel1b_mother_dy*cm $panel1b_mother_dz*cm";
-        #     $detector{"material"} = $material;
-        #     $detector{"visible"} = 1;
-        #     $detector{"style"} = 1;
-        #     $detector{"sensitivity"} = "flux";
-        #     $detector{"hit_type"} = "flux";
-        #     $detector{"identifiers"} = "id manual $identifier";
-        #     print_det(\%configuration, \%detector);
-        # }
-
+	# cone before FTOF
+        my $forward_nplanes = 2;
+        my @forward_iradius = (4500, 300);
+        my @forward_oradius = (4501, 301);
+        my @forward_zpos_root = (4042, 6000);
+    
+        $dimen = "0.0*deg 360*deg $forward_nplanes*counts";
+        for(my $i = 0; $i <$forward_nplanes; $i++) {$dimen = $dimen ." $forward_iradius[$i]*mm";}
+        for(my $i = 0; $i <$forward_nplanes; $i++) {$dimen = $dimen ." $forward_oradius[$i]*mm";}
+        for(my $i = 0; $i <$forward_nplanes; $i++) {$dimen = $dimen ." $forward_zpos_root[$i]*mm";}
+    
+        %detector = init_det();
+    	$detector{"name"}        = "ddvcs_forward_flux";
+    	$detector{"mother"}      = "root";
+    	$detector{"description"} = "ddvcs forward flux";
+    	$detector{"color"}       = "aa0088";
+    	$detector{"type"}        = "Polycone";
+    	$detector{"dimensions"}  = $dimen;
+        $detector{"pos"}         = "0.0*cm 0.0*cm 0.0*cm";
+        $detector{"material"}    = $material;
+        $detector{"style"}       = 1;
+        $detector{"sensitivity"} = "flux";
+        $detector{"hit_type"}    = "flux";
+        $detector{"identifiers"} = "id manual 10";
+        print_det(\%configuration, \%detector);
 
 
     }
