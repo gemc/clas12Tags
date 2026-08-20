6b666d7b83252efca3743a006a74bd5dc5abec77
Ethan Cline <ewcline@mit.edu>
2026-08-20 16:48:43 +0200

    clean up

--- geometry_source/mucal/geometry_java.pl
+++ geometry_source/mucal/geometry_java.pl
@@ -25,7 +25,7 @@ sub makeMUCAL {
 
     ($mothers, $positions, $rotations, $types, $dimensions, $ids) = @main::volumes;
 
-    $panel1a_n = 43;#$main::parameters{"mucal.panel1a.ncounters"};
+    $panel1a_n = 143;#$main::parameters{"mucal.panel1a.ncounters"};
     # $panel1b_n = $main::parameters{"mucal.panel1b.ncounters"};
     # $panel2_n = $main::parameters{"mucal.panel2.ncounters"};
 
@@ -76,52 +76,6 @@ sub build_panel1a_mother {
     }
 }
 
-sub build_panel1b_mother {
-    my $sector = shift;
-
-    my %detector = init_det();
-
-    my $vname = "mucal_p1b_s" . $sector;
-    $detector{"name"} = $vname;
-    $detector{"mother"} = $mothers->{$vname};
-    $detector{"pos"} = $positions->{$vname};
-    $detector{"rotation"} = $rotations->{$vname};
-    $detector{"type"} = $types->{$vname};
-    $detector{"dimensions"} = $dimensions->{$vname};
-
-    $detector{"description"} = " MUCAL - Panel 1b - Sector " . $sector;
-    $detector{"color"} = "000000";
-    $detector{"material"} = "G4_AIR";
-    $detector{"mfield"} = "no";
-    $detector{"ncopy"} = "1";
-    $detector{"visible"} = 0;
-    $detector{"style"} = 0;
-    print_det(\%main::configuration, \%detector);
-}
-
-sub build_panel2_mother {
-    my $sector = shift;
-
-    my %detector = init_det();
-
-    my $vname = "mucal_p2_s" . $sector;
-    $detector{"name"} = $vname;
-    $detector{"mother"} = $mothers->{$vname};
-    $detector{"pos"} = $positions->{$vname};
-    $detector{"rotation"} = $rotations->{$vname};
-    $detector{"type"} = $types->{$vname};
-    $detector{"dimensions"} = $dimensions->{$vname};
-
-    $detector{"description"} = " MUCAL - Panel 2 - Sector " . $sector;
-    $detector{"color"} = "000000";
-    $detector{"material"} = "G4_AIR";
-    $detector{"mfield"} = "no";
-    $detector{"ncopy"} = "1";
-    $detector{"visible"} = 0;
-    $detector{"style"} = 0;
-    print_det(\%main::configuration, \%detector);
-}
-
 # Paddles
 sub build_counters {
     for (my $s = 1; $s <= 1; $s++) {
@@ -163,87 +117,7 @@ sub build_panel1a_counters {
     }
 }
 
-sub build_panel1b_counters {
-    my $sector = shift;
-    my $mother = "mucal_p1b_s" . $sector;
 
-    for (my $n = 1; $n <= $panel1b_n; $n++) {
-        my %detector = init_det();
 
-        my $vname = "panel1b_sector$sector" . "_paddle_" . $n;
-        $detector{"name"} = $vname;
-        $detector{"mother"} = $mothers->{$vname};
-        $detector{"pos"} = $positions->{$vname};
-        $detector{"rotation"} = $rotations->{$vname};
-        $detector{"type"} = $types->{$vname};
-        $detector{"dimensions"} = $dimensions->{$vname};
-
-        $detector{"description"} = "paddle $n - Panel 1B - Sector $sector";
-        $detector{"color"} = "11ffaa";
-        $detector{"material"} = "G4_PbWO4";
-        $detector{"mfield"} = "no";
-        $detector{"visible"} = 1;
-        $detector{"style"} = 1;
-        $detector{"sensitivity"} = "mucal";
-        $detector{"hit_type"} = "mucal";
-        $detector{"identifiers"} = "sector manual $sector panel manual 2 paddle manual $n side manual 0";
-        print_det(\%main::configuration, \%detector);
-    }
-}
-
-sub build_panel2_counters {
-    my $sector = shift;
-    my $mother = "mucal_p2_s" . $sector;
-
-    for (my $n = 1; $n <= $panel2_n; $n++) {
-        my %detector = init_det();
-
-        my $vname = "panel2_sector$sector" . "_paddle_" . $n;
-        $detector{"name"} = $vname;
-        $detector{"mother"} = $mothers->{$vname};
-        $detector{"pos"} = $positions->{$vname};
-        $detector{"rotation"} = $rotations->{$vname};
-        $detector{"type"} = $types->{$vname};
-        $detector{"dimensions"} = $dimensions->{$vname};
-
-        $detector{"description"} = "paddle $n - Panel 2 - Sector $sector";
-        $detector{"color"} = "ff11aa";
-        $detector{"material"} = "scintillator";
-        $detector{"mfield"} = "no";
-        $detector{"visible"} = 1;
-        $detector{"style"} = 1;
-        $detector{"sensitivity"} = "mucal";
-        $detector{"hit_type"} = "mucal";
-        $detector{"identifiers"} = "sector manual $sector  panel manual 3  paddle manual $n side manual 0";
-        print_det(\%main::configuration, \%detector);
-    }
-}
-
-sub make_pb {
-    # loop over sectors
-    for (my $isect = 0; $isect < 6; $isect++) {
-        my $sector = $isect + 1;
-
-        my @panels = ("1a", "2");
-        foreach my $pan (@panels) {
-            my %detector = init_det();
-
-            my $vname = "mucal_shield_p$pan" . "_sector$sector";
-            $detector{"name"} = $vname;
-            $detector{"mother"} = $mothers->{$vname};
-            $detector{"pos"} = $positions->{$vname};
-            $detector{"rotation"} = $rotations->{$vname};
-            $detector{"type"} = $types->{$vname};
-            $detector{"dimensions"} = $dimensions->{$vname};
-
-            $detector{"description"} = "Layer of lead - layer $pan - Sector $sector";
-            $detector{"color"} = "dc143c";
-            $detector{"material"} = "G4_Pb";
-            $detector{"visible"} = 1;
-            $detector{"style"} = 1;
-            print_det(\%main::configuration, \%detector);
-        }
-    }
-}
 
 1;
